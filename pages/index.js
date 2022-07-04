c740106bca6ae74febec3c4501a740770f932da1
agusruidiazgd <agusruidiaz@hotmail.com>
2022-07-04 22:55:32 +0200

    :sparkles: setup prettier

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,5 @@
 // import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
